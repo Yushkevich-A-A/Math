8e22fd2dd1e39d6aee476c4579669ecb71e84993
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-05-15 21:10:58 +0300

    lint 100%

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import './characters/Bowerman';
+import './characters/Daemon';
+import './characters/Magician';
+import './characters/Swordsman';
+import './characters/Undead';
+import './characters/Zombie';
